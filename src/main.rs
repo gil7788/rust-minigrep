5f2ebc9c50aa8bdfab660108770e2c560a58187c
gil7788 <gil7788@gmail.com>
2024-02-25 17:16:14 +0200

    Read File from CLI Path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,5 +12,11 @@ fn main() {
     println!("Searching for {}", query);
     println!("In file {}", file_path);
     println!("Hello, world!");
+
+    println!("in file {}", file_path);
+    let contents = fs::read_to_string(file_path)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
 
