e494cdcd246e7d71100d66654af01d418aeb0275
gil7788 <gil7788@gmail.com>
2024-02-25 17:11:39 +0200

    Read Args from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
     println!("Hello, world!");
 }
 
