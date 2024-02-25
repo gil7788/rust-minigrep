9d5bdb097649a4d65934214bd6310a04437ecb28
gil7788 <gil7788@gmail.com>
2024-02-25 18:59:57 +0200

    Refactor Code Into Functions

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
 use std::env;
 use std::fs;
+use std::error::Error;
 
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-    println!("Hello, world!");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("in file {}", file_path);
-    let contents = fs::read_to_string(file_path)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+   let result = run(config);
+    if let Err(e) = result {
+         println!("Application error: {}", e);
+         std::process::exit(1);
+    }
 }
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
