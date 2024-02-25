b72284c54da7072f04bf0fd5912d153fb0e33bc1
gil7788 <gil7788@gmail.com>
2024-02-25 19:09:50 +0200

    Export Logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
 use std::env;
-use std::fs;
-use std::error::Error;
+use minigrep::Config;
 
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -31,16 +14,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-   let result = run(config);
+   let result = minigrep::run(config);
     if let Err(e) = result {
          println!("Application error: {}", e);
          std::process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
